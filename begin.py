e8eb6296aee522ae5edcfbc8e90bf60a3a99b073
dkopa <dr.kopa.shamsu@gmail.com>
2017-10-31 09:20:35 +0100

    reorganize

--- begin.py
+++ begin.py
@@ -6,10 +6,6 @@ Created on Mon Oct 23 23:48:12 2017
 @author: hasnat
 """
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import itertools
 
 import pandas as pd
@@ -48,3 +44,82 @@ test.drop('Id',axis = 1, inplace = True)
 
 print("")
 print("List of features contained our dataset:",list(train.columns))
+
+from sklearn.ensemble import IsolationForest
+
+clf = IsolationForest(max_samples = 100, random_state = 42)
+clf.fit(train)
+y_noano = clf.predict(train)
+y_noano = pd.DataFrame(y_noano, columns = ['Top'])
+y_noano[y_noano['Top'] == 1].index.values
+
+train = train.iloc[y_noano[y_noano['Top'] == 1].index.values]
+train.reset_index(drop = True, inplace = True)
+print("Number of Outliers:", y_noano[y_noano['Top'] == -1].shape[0])
+print("Number of rows without outliers:", train.shape[0])
+
+col_train = list(train.columns)
+col_train_bis = list(train.columns)
+
+col_train_bis.remove('SalePrice')
+
+mat_train = np.matrix(train)
+mat_test  = np.matrix(test)
+mat_new = np.matrix(train.drop('SalePrice',axis = 1))
+mat_y = np.array(train.SalePrice).reshape((1314,1))
+
+prepro_y = MinMaxScaler()
+prepro_y.fit(mat_y)
+
+prepro = MinMaxScaler()
+prepro.fit(mat_train)
+
+prepro_test = MinMaxScaler()
+prepro_test.fit(mat_new)
+
+train = pd.DataFrame(prepro.transform(mat_train),columns = col_train)
+test  = pd.DataFrame(prepro_test.transform(mat_test),columns = col_train_bis)
+
+train.head()
+
+X_train = train.iloc[:,0:-1]
+y_train = np.log1p(mat_y)
+
+##
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.model_selection import cross_val_score
+
+def rmse_cv(model):
+    rmse= np.sqrt(-cross_val_score(model, X_train, y_train, scoring="neg_mean_squared_error", cv = 5))
+    return(rmse)
+
+# Ridge
+alpha_vals = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000]
+rmse_ridge = [rmse_cv(Ridge(alpha=alphas)).mean() for alphas in alpha_vals]
+print('rmse [Ridge]: ' + str(np.min(pp)))
+
+#myRidge = Ridge(alpha=alpha_vals[np.argmin(rmse_ridge)])
+#myRidge.fit(X_train, y_train)
+#preds = np.expm1(myRidge.predict(test))
+
+#solution = pd.DataFrame({"id":ID, "SalePrice":preds})
+#solution.to_csv("ridge_sol.csv", index = False)
+
+# Lasso
+alpha_vals = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000]
+rmse_lasso = [rmse_cv(Lasso(alpha=alphas)).mean() for alphas in alpha_vals]
+print('rmse [Lasso]: ' + str(np.min(rmse_lasso)))
+
+# SVC
+from sklearn.svm import SVR
+
+c_vals = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000]
+rmse_svr = [rmse_cv(SVR(C=c_val)).mean() for c_val in c_vals]
+print('rmse [SVR]: ' + str(np.min(rmse_svr)))
+
+# Linear regression
+from sklearn.linear_model import LinearRegression
+#lr = LinearRegression()
+#lr.fit(X_train, y_train)
+rmse_lr = rmse_cv(LinearRegression()).mean()
+print('rmse [LR]: ' + str(np.min(rmse_lr)))
